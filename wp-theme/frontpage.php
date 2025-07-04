c1cdefcde584868f13738f3c54c0a9e0d5b34848
Adam Lean <adam.lean.92@gmail.com>
2025-07-04 23:04:55 +0800

    .

---
+++ wp-theme/frontpage.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<section class="hero">
+        <div class="container">
+          <div class="hero-content">
+            <div class="hero-text">
+              <h2 class="subheading">Wishbone+Partners</h2>
+              <h1 class="heading">Дом<br />прекрасной архитектуры.</h1>
+              <p class="description">
+                Мы — архитектурная фирма, которая фокусируется на красивом, но
+                функциональном дизайне. По сути, мы считаем, что дизайн — это
+                удобство использования и доступность — это руководящие принципы
+                нашей работы. Подробнее о наших предыдущих проектах, нашем
+                процессе и нашей команде читайте ниже.
+              </p>
+              <a href="#" class="read-more">Узнать больше</a>
+            </div>
+            <div class="hero-image">
+              <img src="<?php echo get_template_directory_uri(); ?>./img/hero_image.png" alt="hero image" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+<?php get_footer(); ?>
